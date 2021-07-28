81ce909866568919571d30b2b5a48357d4c3aa10
Guionardo Furlan <guionardo.furlan@ambevtech.com.br>
2021-07-28 13:52:40 -0300

    New version

---
+++ lasier/src/pages/Index.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      @click="playSound"
+      alt="Quasar logo"
+      src="~assets/electric.svg"
+      style="width: 60%"
+    >
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+const sound = new Audio();
+sound.src = '~assets/lasier.mp3';
+
+export default defineComponent({
+  name: 'PageIndex',
+  setup () {
+    return {
+      sound: new Audio()
+
+    }
+  },
+  mounted () {
+    this.sound = new Audio()
+    this.sound.src = require('../assets/lasier.mp3')
+  },
+  methods: {
+    playSound () {
+      this.sound.play()
+    }
+  }
+})
+</script>
